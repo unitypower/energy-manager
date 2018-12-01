7af73353073b122404c130a080255871710a4f52
unitypower <43620760+unitypower@users.noreply.github.com>
2018-12-01 21:36:55 +0000

    Add files via upload

---
+++ Agile_Parser.cpp
@@ -0,0 +1,68 @@
+//============================================================================
+// Name        : Agile_Parser.cpp
+// Author      : Jim McKenzie
+// Version     :
+// Copyright   : Your copyright notice
+// Description :
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+
+using namespace std;
+
+#define PORT 80
+
+int main(int argc, char const *argv[])
+{
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "GET /v1/products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-A/standard-unit-rates/ HTTP/1.1 \r\n"
+    		" Host: api.octopus.energy\r\n"
+    		" Authorization: Basic c2tfbGl2ZV8zQkRWajZkRWptS3M5bWpYbGpmeW9WWnk6\r\n cache-control: no-cache";
+
+    char buffer[2024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "34.246.255.75", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+
+
+
+    char cur;
+        while ( recv( sock , buffer, 2024,0) > 0 ) {
+        	   printf("%s\n",buffer );
+        }
+    return 0;
+}
+
+
+
